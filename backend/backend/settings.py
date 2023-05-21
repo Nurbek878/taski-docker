07ffd280a305b6d78e4b0ec8ecc1e40bbf3c96de
Нурбек Орозалиев <nurik878878@yandex.ru>
2023-05-21 21:56:33 +0500

    Edit for flake8

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -119,6 +119,4 @@ STATIC_ROOT = BASE_DIR / 'collected_static'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ORIGIN_WHITELIST = [
-     'http://localhost:3000'
-]
+CORS_ORIGIN_WHITELIST = ['http://localhost:3000']
